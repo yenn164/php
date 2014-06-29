3281a12072b6724dec3e67d4a5c141dfdfd733a9
gonzalojota <gonzalo.jota@gmail.com>
2014-06-29 18:06:25 -0300

    Nuevo sql con claves foraneas

--- Resource/testdrive.sql
+++ Resource/testdrive.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 18-06-2014 a las 22:51:39
+-- Tiempo de generación: 29-06-2014 a las 18:03:42
 -- Versión del servidor: 5.5.35-0ubuntu0.12.04.2
 -- Versión de PHP: 5.3.10-1ubuntu3.11
 
@@ -40,6 +40,11 @@ CREATE TABLE IF NOT EXISTS `CLIENTE` (
   PRIMARY KEY (`idCliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Truncar tablas antes de insertar `CLIENTE`
+--
+
+TRUNCATE TABLE `CLIENTE`;
 -- --------------------------------------------------------
 
 --
@@ -49,27 +54,56 @@ CREATE TABLE IF NOT EXISTS `CLIENTE` (
 DROP TABLE IF EXISTS `INMUEBLE`;
 CREATE TABLE IF NOT EXISTS `INMUEBLE` (
   `idinmueble` int(4) NOT NULL AUTO_INCREMENT,
-  `fechaPublicacion` date NOT NULL,
-  `gastosComunes` decimal(6,0) NOT NULL,
-  `superEdif` decimal(6,0) NOT NULL,
-  `anioConst` int(4) NOT NULL,
-  `dormitorios` int(2) NOT NULL,
-  `banios` int(2) NOT NULL,
-  `cocina` int(2) NOT NULL,
-  `living` int(2) NOT NULL,
-  `comedor` int(2) NOT NULL,
-  `terraza` int(2) NOT NULL,
-  `piso` int(2) NOT NULL,
-  `equipado` tinyint(1) NOT NULL,
-  `padron` int(8) NOT NULL,
-  `mejoras` tinyint(1) NOT NULL,
-  `nivelado` tinyint(1) NOT NULL,
-  `agreste` tinyint(1) NOT NULL,
-  `tipo` varchar(10) NOT NULL,
+  `fechaPublicacion` date DEFAULT NULL,
+  `gastosComunes` decimal(6,0) DEFAULT NULL,
+  `superEdif` decimal(6,0) DEFAULT NULL,
+  `anioConst` int(4) DEFAULT NULL,
+  `dormitorios` int(2) DEFAULT NULL,
+  `banios` int(2) DEFAULT NULL,
+  `cocina` int(2) DEFAULT NULL,
+  `living` int(2) DEFAULT NULL,
+  `comedor` int(2) DEFAULT NULL,
+  `terraza` int(2) DEFAULT NULL,
+  `piso` int(2) DEFAULT NULL,
+  `equipado` tinyint(1) DEFAULT NULL,
+  `padron` int(8) DEFAULT NULL,
+  `mejoras` tinyint(1) DEFAULT NULL,
+  `nivelado` tinyint(1) DEFAULT NULL,
+  `agreste` tinyint(1) DEFAULT NULL,
+  `tipo` varchar(10) DEFAULT NULL,
   `idubicacion` int(4) NOT NULL,
-  PRIMARY KEY (`idinmueble`)
+  PRIMARY KEY (`idinmueble`),
+  UNIQUE KEY `Iidubicacion` (`idubicacion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Truncar tablas antes de insertar `INMUEBLE`
+--
+
+TRUNCATE TABLE `INMUEBLE`;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `TRANSACION`
+--
+
+DROP TABLE IF EXISTS `TRANSACION`;
+CREATE TABLE IF NOT EXISTS `TRANSACION` (
+  `idTrans` int(4) NOT NULL AUTO_INCREMENT,
+  `tipoTrans` int(3) NOT NULL,
+  `fechaTrans` date NOT NULL,
+  `idCliente` int(4) NOT NULL,
+  `idInmueble` int(4) NOT NULL,
+  PRIMARY KEY (`idTrans`),
+  UNIQUE KEY `IidCliente` (`idCliente`),
+  UNIQUE KEY `IidInmueble` (`idInmueble`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Truncar tablas antes de insertar `TRANSACION`
+--
+
+TRUNCATE TABLE `TRANSACION`;
 -- --------------------------------------------------------
 
 --
@@ -79,16 +113,24 @@ CREATE TABLE IF NOT EXISTS `INMUEBLE` (
 DROP TABLE IF EXISTS `UBICACION`;
 CREATE TABLE IF NOT EXISTS `UBICACION` (
   `idubicacion` int(4) NOT NULL AUTO_INCREMENT,
-  `latitud` int(8) NOT NULL,
-  `longitud` int(8) NOT NULL,
-  `zona` varchar(40) NOT NULL,
-  `barrio` varchar(40) NOT NULL,
-  `ciudad` varchar(40) NOT NULL,
-  `departamento` varchar(40) NOT NULL,
-  `descripcion` varchar(40) NOT NULL,
+  `latitud` int(8) DEFAULT NULL,
+  `longitud` int(8) DEFAULT NULL,
+  `zona` varchar(40) DEFAULT NULL,
+  `barrio` varchar(40) DEFAULT NULL,
+  `ciudad` varchar(40) DEFAULT NULL,
+  `departamento` varchar(40) DEFAULT NULL,
+  `descripcion` varchar(40) DEFAULT NULL,
+  `calle` varchar(30) DEFAULT NULL,
+  `numero` int(5) DEFAULT NULL,
+  `apto` int(4) DEFAULT NULL,
   PRIMARY KEY (`idubicacion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Truncar tablas antes de insertar `UBICACION`
+--
+
+TRUNCATE TABLE `UBICACION`;
 -- --------------------------------------------------------
 
 --
@@ -104,6 +146,11 @@ CREATE TABLE IF NOT EXISTS `USER` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
+-- Truncar tablas antes de insertar `USER`
+--
+
+TRUNCATE TABLE `USER`;
+--
 -- Volcado de datos para la tabla `USER`
 --
 
@@ -111,6 +158,23 @@ INSERT INTO `USER` (`id`, `nombre`, `password`) VALUES
 (1, 'admin', 'admin'),
 (2, 'gonzalo', 'jota'),
 (3, 'juan', 'juan');
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `INMUEBLE`
+--
+ALTER TABLE `INMUEBLE`
+  ADD CONSTRAINT `INMUEBLE_ibfk_1` FOREIGN KEY (`idubicacion`) REFERENCES `UBICACION` (`idubicacion`);
+
+--
+-- Filtros para la tabla `TRANSACION`
+--
+ALTER TABLE `TRANSACION`
+  ADD CONSTRAINT `TRANSACION_ibfk_2` FOREIGN KEY (`idInmueble`) REFERENCES `INMUEBLE` (`idinmueble`),
+  ADD CONSTRAINT `TRANSACION_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `CLIENTE` (`idCliente`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
