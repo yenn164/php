a1d3e0cff389d8435a80e31b5c219ce794850d87
fernandogsevero <fernandogsevero@gmail.com>
2014-06-29 21:22:38 -0300

    Intentando Arreglar

--- BackOffice/protected/views/site/inmuebles.php
+++ BackOffice/protected/views/site/inmuebles.php
@@ -4,9 +4,9 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>Inmuebles<i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>Inmuebles.</p>
 
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
