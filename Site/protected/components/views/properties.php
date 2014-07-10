e22fb0fe69e15ec9b7a40a0a97379cb46537131e
yenn164 <yenn2005@gmail.com>
2014-07-10 00:43:15 -0300

    desde base

--- Site/protected/components/views/properties.php
+++ Site/protected/components/views/properties.php
@@ -2,33 +2,15 @@
 
 
 <div id="portfoliolist">
-<?php foreach($models as $model): ?>
-    
-    <?php $serchType ='';
-    if($model->type==1){
-    $serchType='casa';}
-    else if($model->type==2){
-    $serchType='apartamento';}
-    else if($model->type==3){
-    $serchType='lcomercial';}
-    else {
-    $serchType ='terreno';}
-    ?>
-    
-    <div class="portfolio logo1" data-cat="<?php $serchType?>">
-            <div class="portfolio-wrapper">				
-                    <a  href="#">
-                            <img src="images/sale_pic1.jpg"  alt="Image 2" />
-                    </a>
-                    <div class="label">
-                            <div class="label-text">
-                                    <a class="text-title"><?php echo $model->fechaPublicacion;?></a>
-                                    <span class="text-category"><?php echo $model->equipado;?></span>
-                            </div>
-                            <div class="label-bg"></div>
-                    </div>
-            </div>
-    </div>	
+    <?php foreach($models as $rows): ?>
+    <?php echo '<td>'.$rows['tipo'].'</td>' ?>
+    <?php echo '<td>'.$rows['descripcion'].'</td>' ?>
+    <br>
+    <?php foreach($rows as $colums): ?>
+    <?php echo '<td>'.$colums.'</td>' ?>
+   
+    <?php endforeach; ?>
+        <BR>
     <?php endforeach; ?>
 </div>
 <?php endif;
